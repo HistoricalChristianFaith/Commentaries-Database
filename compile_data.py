d45307cbd2ff2f0ad5bb7e057826dac14519e1eb
Sean Kooyman <github@teachthe.net>
2022-11-14 13:17:42 -0500

    Update compile_data.py

--- compile_data.py
+++ compile_data.py
@@ -38,77 +38,81 @@ def process_toml():
     for f in filelist:
         if f.endswith(".toml"):
 
-            # Pseudo-Jerome/Mark 6_35-44.toml
-            short_file_name = f.replace(os.getcwd()+"/", "") 
-            if 'metadata.toml' not in short_file_name:
-                for i in range(10):
-                    if(current_file_count == int(total_file_count*(0.1*i))):
-                        print(str(i)+"0% done ("+str(current_file_count)+" / "+str(total_file_count)+")")
-                current_file_count += 1
-                f_pieces = short_file_name.split("/")
-                father_name = f_pieces[0]
-                fn = f_pieces[1].replace(".toml", "")
-                fn_pieces = fn.split(" ")
-                verse_pieces = fn_pieces[-1].split("-")
-                book_name = " ".join(fn_pieces[:-1])
+            try:
+                # Pseudo-Jerome/Mark 6_35-44.toml
+                short_file_name = f.replace(os.getcwd()+"/", "") 
+                if 'metadata.toml' not in short_file_name:
+                    for i in range(10):
+                        if(current_file_count == int(total_file_count*(0.1*i))):
+                            print(str(i)+"0% done ("+str(current_file_count)+" / "+str(total_file_count)+")")
+                    current_file_count += 1
+                    f_pieces = short_file_name.split("/")
+                    father_name = f_pieces[0]
+                    fn = f_pieces[1].replace(".toml", "")
+                    fn_pieces = fn.split(" ")
+                    verse_pieces = fn_pieces[-1].split("-")
+                    book_name = " ".join(fn_pieces[:-1])
 
-                start_verse = verse_pieces[0]
-                start_verse_pieces = start_verse.split("_")
-                start_verse_CHAPTER = start_verse_pieces[0]
-                start_verse_VERSE = start_verse_pieces[1]
-                end_verse_CHAPTER = start_verse_pieces[0]
-                end_verse_VERSE = start_verse_pieces[1]
+                    start_verse = verse_pieces[0]
+                    start_verse_pieces = start_verse.split("_")
+                    start_verse_CHAPTER = start_verse_pieces[0]
+                    start_verse_VERSE = start_verse_pieces[1]
+                    end_verse_CHAPTER = start_verse_pieces[0]
+                    end_verse_VERSE = start_verse_pieces[1]
 
-                if(len(verse_pieces) > 1):
-                    # There is an ending verse
-                    endverse_pieces = verse_pieces[1].split("_");
-                    if(len(endverse_pieces) == 2):
-                        # 19_24
-                        end_verse_CHAPTER = endverse_pieces[0];
-                        end_verse_VERSE = endverse_pieces[1];
-                    else:
-                        # 19, borrows chapter from starting verse
-                        end_verse_VERSE = endverse_pieces[0];
+                    if(len(verse_pieces) > 1):
+                        # There is an ending verse
+                        endverse_pieces = verse_pieces[1].split("_");
+                        if(len(endverse_pieces) == 2):
+                            # 19_24
+                            end_verse_CHAPTER = endverse_pieces[0];
+                            end_verse_VERSE = endverse_pieces[1];
+                        else:
+                            # 19, borrows chapter from starting verse
+                            end_verse_VERSE = endverse_pieces[0];
 
 
-                location_start = (int(start_verse_CHAPTER)*1000000) + int(start_verse_VERSE)
-                location_end = (int(end_verse_CHAPTER)*1000000) + int(end_verse_VERSE)
+                    location_start = (int(start_verse_CHAPTER)*1000000) + int(start_verse_VERSE)
+                    location_end = (int(end_verse_CHAPTER)*1000000) + int(end_verse_VERSE)
 
-                # print(father_name + " / " + book_name + " / " + start_verse_CHAPTER + " / " + start_verse_VERSE + " / " + end_verse_CHAPTER + " / " + end_verse_VERSE + "|||" + str(location_start) + "/" + str(location_end))
+                    # print(father_name + " / " + book_name + " / " + start_verse_CHAPTER + " / " + start_verse_VERSE + " / " + end_verse_CHAPTER + " / " + end_verse_VERSE + "|||" + str(location_start) + "/" + str(location_end))
 
-                with open(f, 'r') as tomlfile:
-                    toml_str = tomlfile.read()
-                toml_obj = tomlkit.parse(toml_str)
+                    with open(f, 'r') as tomlfile:
+                        toml_str = tomlfile.read()
+                    toml_obj = tomlkit.parse(toml_str)
 
-                for c in toml_obj['commentary']:
-                    source_url = ""
-                    source_title = ""
-                    if 'sources' in c:
-                        source_url = c['sources'][0]['url']
-                        source_title = c['sources'][0]['title']
+                    for c in toml_obj['commentary']:
+                        source_url = ""
+                        source_title = ""
+                        if 'sources' in c:
+                            source_url = c['sources'][0]['url']
+                            source_title = c['sources'][0]['title']
 
-                    time = 9999999;
-                    if(father_name in default_data):
-                        time = default_data[father_name]
-                    if('time' in c):
-                        time = c['time']
+                        time = 9999999;
+                        if(father_name in default_data):
+                            time = default_data[father_name]
+                        if('time' in c):
+                            time = c['time']
 
-                    append_to_author_name = "";
-                    if('append_to_author_name' in c):
-                        append_to_author_name = c['append_to_author_name']
-                    
-                    data_values.append([
-                        str(uuid.uuid4()),
-                        father_name,
-                        append_to_author_name,
-                        time,
-                        book_name.lower().replace(" ", ""),
-                        location_start,
-                        location_end,
-                        c['quote'],
-                        source_url,
-                        source_title
-                    ])
+                        append_to_author_name = "";
+                        if('append_to_author_name' in c):
+                            append_to_author_name = c['append_to_author_name']
+                        
+                        data_values.append([
+                            str(uuid.uuid4()),
+                            father_name,
+                            append_to_author_name,
+                            time,
+                            book_name.lower().replace(" ", ""),
+                            location_start,
+                            location_end,
+                            c['quote'],
+                            source_url,
+                            source_title
+                        ])
+            except:
+                print("******Error in", f)
+                raise
     return data_values          
 
 def output_sqlite():
